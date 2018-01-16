24c278c6b837a425cc7eedc215c46e12d6a4051d
Valerie Wood <valeriewd1@gmail.com>
2018-01-16 06:27:39 +0000

    Done.

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function happyHolidays() {
 console.log(a);
 
  /* Write a function named happyHolidayTo. This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well. This function should return the string `Happy ${holiday}, ${name}!` */
-function happyHolidaysTo(name) {
-  var nameto = "PeopleBornInJanuary!";
-  return "Happy holidays!" + name + "!";
+function happyHolidayTo(holiday, name) {
+  var holidayName = happyHolidayTo;
+  return holiday + name;
 }
 
-function happyHolidayTo(holiday, name) {
-  return "Happy" + holiday, name + "!";
-}
+happyHolidayTo("Happy Birthday ", "Janet!");
+
+
 
 
 function holidayCountdown(holiday, days) {
